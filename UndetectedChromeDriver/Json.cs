90738d231cff77e8e48e77201f0b8ab5d3e24cbe
fysh711426 <fysh711426@hotmail.com>
2023-02-23 10:20:14 +0800

    update Json

--- UndetectedChromeDriver/Json.cs
+++ UndetectedChromeDriver/Json.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,15 +10,33 @@ namespace SeleniumUndetectedChromeDriver
     {
         public static Dictionary<string, object> DeserializeData(string data)
         {
-            return DeserializeData(
-                JsonConvert.DeserializeObject<Dictionary<string, object>>(data))
-                as Dictionary<string, object>;
+            var obj = JsonConvert.DeserializeObject<Dictionary<string, object>>(data);
+            if (obj == null)
+                throw new Exception("Json data cannot be null.");
+            return (Dictionary<string, object>)DeserializeData(obj);
         }
 
         private static IDictionary<string, object> DeserializeData(JObject data)
         {
             return DeserializeData(
-                data.ToObject<Dictionary<string, object>>());
+                data.ToObject<Dictionary<string, object>>() ?? new());
+        }
+
+        private static IList<object> DeserializeData(JArray data)
+        {
+            var list = data.ToObject<List<object>>() ?? new();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var value = list[i];
+
+                if (value is JObject)
+                    list[i] = DeserializeData((JObject)value);
+
+                if (value is JArray)
+                    list[i] = DeserializeData((JArray)value);
+            }
+            return list;
         }
 
         private static IDictionary<string, object> DeserializeData(IDictionary<string, object> data)
@@ -27,29 +46,12 @@ namespace SeleniumUndetectedChromeDriver
                 var value = data[key];
 
                 if (value is JObject)
-                    data[key] = DeserializeData(value as JObject);
+                    data[key] = DeserializeData((JObject)value);
 
                 if (value is JArray)
-                    data[key] = DeserializeData(value as JArray);
+                    data[key] = DeserializeData((JArray)value);
             }
             return data;
-        }
-
-        private static IList<object> DeserializeData(JArray data)
-        {
-            var list = data.ToObject<List<object>>();
-
-            for (int i = 0; i < list.Count; i++)
-            {
-                var value = list[i];
-
-                if (value is JObject)
-                    list[i] = DeserializeData(value as JObject);
-
-                if (value is JArray)
-                    list[i] = DeserializeData(value as JArray);
-            }
-            return list;
         }
     }
 }
