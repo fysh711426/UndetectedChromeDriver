ca188e1b24d814c3911b19ec91207e52662c23b7
thuchung.ha <ha.thuchung96@gmail.com>
2023-08-21 02:25:58 +0700

    [Implement] Get dynamic lastest version stable chromedriver

--- UndetectedChromeDriver/ChromeDriverInstaller.cs
+++ UndetectedChromeDriver/ChromeDriverInstaller.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
@@ -12,20 +13,42 @@ namespace SeleniumUndetectedChromeDriver
     // refer: https://swimburger.net/blog/dotnet/download-the-right-chromedriver-version-and-keep-it-up-to-date-on-windows-linux-macos-using-csharp-dotnet
     public class ChromeDriverInstaller
     {
-        public async Task<string> Auto(string? browserExecutablePath = null, bool force = false)
+        public async Task<string> Auto(string browserExecutablePath = null, bool force = false)
         {
-            var version = await new ChromeExecutable()
-                .GetVersion(browserExecutablePath);
-            return await Install(version, force);
+            return await Install(await ChromeDriverInstaller.GetDynamicVersionAsync(), force);
         }
-
+        private static async Task<string> GetDynamicVersionAsync()
+        {
+            HttpClient client = new HttpClient();   // actually only one object should be created by Application
+            using (HttpResponseMessage response = await client.GetAsync("https://googlechromelabs.github.io/chrome-for-testing/"))
+            {
+                using (HttpContent content = response.Content)
+                {
+                    string pageContent = await content.ReadAsStringAsync();
+                    string searchtext = "chrome/chrome-for-testing/";
+                    int firstid = pageContent.IndexOf(searchtext);
+                    if (firstid != -1)
+                    {
+                        firstid = firstid + searchtext.Count();
+                        int lastid = firstid;
+                        while (lastid < pageContent.Count() && pageContent[lastid] != '/')
+                        {
+                            lastid++;
+                        }
+                        return pageContent.Substring(firstid, lastid - firstid);
+                    }
+                }
+            }
+            return String.Empty;
+        }
         public async Task<string> Install(string version, bool force = false)
         {
             if (string.IsNullOrWhiteSpace(version))
                 throw new Exception("Parameter version is required.");
-            version = version.Substring(0, version.LastIndexOf('.'));
+            // version = version.Substring(0, version.LastIndexOf('.'));
 
             var zipName = "";
+            var path = "";
             var driverName = "";
             var tempPath = "";
             var ext = "";
@@ -38,21 +61,24 @@ namespace SeleniumUndetectedChromeDriver
 #else
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                zipName = "chromedriver_win32.zip";
-                driverName = $"chromedriver_{version}.exe";
+                zipName = "chromedriver-win64.zip";
+                path = "win64";
+                driverName = $"chromedriver-win64";
                 tempPath = "AppData/Roaming/UndetectedChromeDriver";
                 ext = ".exe";
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                zipName = "chromedriver_linux64.zip";
-                driverName = $"chromedriver_{version}";
+                zipName = "chromedriver-linux64.zip";
+                path = "linux64";
+                driverName = $"chromedriver-linux64";
                 tempPath = ".local/share/UndetectedChromeDriver";
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                zipName = "chromedriver_mac64.zip";
-                driverName = $"chromedriver_{version}";
+                zipName = "chromedriver-mac-x64.zip";
+                path = "mac-x64";
+                driverName = $"chromedriver-mac-x64";
                 tempPath = "Library/Application Support/UndetectedChromeDriver";
             }
             else
@@ -67,103 +93,94 @@ namespace SeleniumUndetectedChromeDriver
             if (!force && File.Exists(driverPath))
                 return driverPath;
 
-            var httpClient = Http.Client;
-            httpClient.BaseAddress = new Uri("https://chromedriver.storage.googleapis.com/");
-
-            // Append the result to URL "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_". 
-            // For example, with Chrome version 72.0.3626.81, you'd get a URL "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_72.0.3626".
-            var versionResponse = await httpClient.GetAsync($"LATEST_RELEASE_{version}");
-            if (!versionResponse.IsSuccessStatusCode)
+            using (var httpClient = new HttpClient())
             {
-                if (versionResponse.StatusCode == HttpStatusCode.NotFound)
-                    throw new Exception($"ChromeDriver version not found for Chrome version {version}");
-                else
-                    throw new Exception($"ChromeDriver version request failed with status code: {versionResponse.StatusCode}, reason phrase: {versionResponse.ReasonPhrase}");
-            }
+                httpClient.BaseAddress = new Uri($"https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/");
 
-            var driverVersion = await versionResponse.Content.ReadAsStringAsync();
 
-            var dirPath = Path.GetDirectoryName(driverPath);
-            if (dirPath == null)
-                throw new Exception("Get ChromeDriver directory faild.");
-            if (!Directory.Exists(dirPath))
-                Directory.CreateDirectory(dirPath);
+                var dirPath = Path.GetDirectoryName(driverPath);
+                if (dirPath == null)
+                    throw new Exception("Get ChromeDriver directory faild.");
+                if (!Directory.Exists(dirPath))
+                    Directory.CreateDirectory(dirPath);
 
-            if (force)
-            {
-                void deleteDriver()
+                if (force)
                 {
-                    if (File.Exists(driverPath))
-                        File.Delete(driverPath);
-                }
-                try
-                {
-                    deleteDriver();
-                }
-                catch
-                {
+                    void deleteDriver()
+                    {
+                        if (File.Exists(driverPath))
+                            File.Delete(driverPath);
+                    }
                     try
                     {
-                        await forceKillInstances(driverPath);
                         deleteDriver();
                     }
-                    catch { }
+                    catch
+                    {
+                        try
+                        {
+                            await forceKillInstances(driverPath);
+                            deleteDriver();
+                        }
+                        catch { }
+                    }
                 }
-            }
 
-            // Use the URL created in the last step to retrieve a small file containing the version of ChromeDriver to use. For example, the above URL will get your a file containing "72.0.3626.69". (The actual number may change in the future, of course.)
-            // Use the version number retrieved from the previous step to construct the URL to download ChromeDriver. With version 72.0.3626.69, the URL would be "https://chromedriver.storage.googleapis.com/index.html?path=72.0.3626.69/".
-            var zipResponse = await httpClient.GetAsync($"{driverVersion}/{zipName}");
-            if (!zipResponse.IsSuccessStatusCode)
-                throw new Exception($"ChromeDriver download request failed with status code: {zipResponse.StatusCode}, reason phrase: {zipResponse.ReasonPhrase}");
+                // Use the URL created in the last step to retrieve a small file containing the version of ChromeDriver to use. For example, the above URL will get your a file containing "72.0.3626.69". (The actual number may change in the future, of course.)
+                // Use the version number retrieved from the previous step to construct the URL to download ChromeDriver. With version 72.0.3626.69, the URL would be "https://chromedriver.storage.googleapis.com/index.html?path=72.0.3626.69/".
+                var zipResponse = await httpClient.GetAsync($"{version}/{path}/{zipName}");
+                if (!zipResponse.IsSuccessStatusCode)
+                    throw new Exception($"ChromeDriver download request failed with status code: {zipResponse.StatusCode}, reason phrase: {zipResponse.ReasonPhrase}");
 
-            // this reads the zipfile as a stream, opens the archive, 
-            // and extracts the chromedriver executable to the targetPath without saving any intermediate files to disk
-            using (var zipStream = await zipResponse.Content.ReadAsStreamAsync())
-            using (var zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Read))
-            using (var fs = new FileStream(driverPath, FileMode.Create))
-            {
-                var entryName = $"chromedriver{ext}";
-                var entry = zipArchive.GetEntry(entryName);
-                if (entry == null)
-                    throw new Exception($"Not found zip entry {entryName}.");
-                using (var stream = entry.Open())
+                // this reads the zipfile as a stream, opens the archive, 
+                // and extracts the chromedriver executable to the targetPath without saving any intermediate files to disk
+                using (var zipStream = await zipResponse.Content.ReadAsStreamAsync())
+                using (var zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Read))
+                using (var fs = new FileStream(driverPath, FileMode.Create))
                 {
-                    await stream.CopyToAsync(fs);
+                    var entryName = $"{driverName}/chromedriver{ext}";
+                    var entry = zipArchive.GetEntry(entryName);
+                    if (entry == null)
+                        throw new Exception($"Not found zip entry {entryName}.");
+                    using (var stream = entry.Open())
+                    {
+                        await stream.CopyToAsync(fs);
+                    }
                 }
-            }
 
 #if (NET48 || NET47 || NET46 || NET45)
 #else
-            // on Linux/macOS, you need to add the executable permission (+x) to allow the execution of the chromedriver
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                var args = @$"+x ""{driverPath}""";
-                var info = new ProcessStartInfo("chmod", args);
-                info.CreateNoWindow = true;
-                info.UseShellExecute = false;
-                info.RedirectStandardOutput = true;
-                info.RedirectStandardError = true;
-                var process = Process.Start(info);
-                if (process == null)
-                    throw new Exception("Process start error.");
-                try
+                // on Linux/macOS, you need to add the executable permission (+x) to allow the execution of the chromedriver
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                 {
-                    var error = await process.StandardError.ReadToEndAsync();
-                    await process.WaitForExitPatchAsync();
-                    process.Kill();
-                    process.Dispose();
+                    var args = $@"+x ""{driverPath}""";
+                    var info = new ProcessStartInfo("chmod", args);
+                    info.CreateNoWindow = true;
+                    info.UseShellExecute = false;
+                    info.RedirectStandardOutput = true;
+                    info.RedirectStandardError = true;
+                    var process = Process.Start(info);
+                    if (process == null)
+                        throw new Exception("Process start error.");
+                    try
+                    {
+                        var error = await process.StandardError.ReadToEndAsync();
+                        await process.WaitForExitPatchAsync();
+                        process.Kill();
+                        process.Dispose();
 
-                    if (!string.IsNullOrEmpty(error))
-                        throw new Exception("Failed to make chromedriver executable.");
+                        if (!string.IsNullOrEmpty(error))
+                            throw new Exception("Failed to make chromedriver executable.");
+                    }
+                    catch
+                    {
+                        process.Dispose();
+                        throw;
+                    }
                 }
-                catch
-                {
-                    process.Dispose();
-                    throw;
-                }
-            }
 #endif
+            }
+
             return driverPath;
         }
 
@@ -171,7 +188,7 @@ namespace SeleniumUndetectedChromeDriver
         {
             if (driverExecutablePath == null)
                 throw new Exception("Parameter driverExecutablePath is required.");
-            
+
             var args = "--version";
             var info = new ProcessStartInfo(driverExecutablePath, args);
             info.CreateNoWindow = true;
